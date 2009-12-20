6c37cbee4baf0bbbf36e9ec22b1a2c47b95c2082
Logan Bell <loganbell@gmail.com>
2009-12-20 03:40:25 -0500

    Various updates to views/tests

--- lib/Views/AbstractView.pm
+++ lib/Views/AbstractView.pm
@@ -2,6 +2,18 @@ package Views::AbstractView;
 
 
 #-------------------------------------------
+# CONSTANT
+#-------------------------------------------
+
+use constant DEFAULT_TITLE         => scalar 'Logan Bell';
+use constant DEFAULT_STYLE_SHEET   => scalar '/styles/base.css';
+use constant META_KEYWORDS         => scalar 'Logan Bell';
+use constant META_DESCRIPTION      => scalar 'Yawn';
+use constant META_AUTHOR           => scalar 'Logan Bell';
+use constant META_ROBOTS           => scalar 'all';
+use constant META_COPYRIGHT        => scalar 'Copyright 2009 (c) by Logan Bell';
+
+#-------------------------------------------
 # PUBLIC METHODS
 #-------------------------------------------
 
@@ -12,34 +24,124 @@ sub html_header
 {
     my ($self) = @_;
     
+    my $css_markup  = $self->_generate_css;
+    my $meta_markup = $self->_generate_meta;
+    my $title       = $self->title;
+    my $html_navbar = $self->html_navbar;
+
     my $html = $self->{cgi_obj}->header;
     $html .= qq~
 <?xml version="1.0" encoding="utf-8" ?> 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" >
 <head>
-	<title>Logan Bell</title>
-
-	<meta name="keywords" content="loganbell logan bell log blog bash perl ruby"/>
-    	<meta name="description" content="Yawn... "/>
-    	<meta name="author" content="Logan Bell" />
-    	<meta name="robots" content="all" />
-	<meta name="copyright" content="Copyright 2009 (c) by Logan Bell" />
-
-	<link rel="stylesheet" href="/styles/base.css" type="text/css" />
+	<title>$title</title>
+    $meta_markup
+    $css_markup
 </head>
 <body>
 <div id="page">
 	<div id="header">
-		<div id="title"><h1>Logan's Log</h1></div>
+		<div id="title"><h1>$title</h1></div>
 		<div id="subtitle">let's converse!</div>
 	</div>
 	<div id="yellowbar"></div>	
 	<div id="wrapper">
-        <%= import('navbar.html') %>
+        $html_navbar
 		<div id="content">~;
 
     return $html;
+}
+
+sub html_navbar
+# Purpose:  Html for navbar
+# Input:    1. Class name
+# Output:   1. String of html
+{
+    my ($self) = @_;
+
+    my $html = qq~
+		<div id="navbar">
+			<div class="headerbox_orange">entrees</div>
+			<div class="box_brown">
+				<ul class="dkarrow">
+					<li><a href="/">blog</a></li>
+					<li><a href="/pages/misc/resume">resume</a></li>
+					<li><a href="/pages/misc/about">about</a></li>
+				</ul>
+			</div>
+			<!--<div class="headerbox_orange">categories</div>
+			<div class="box_brown">
+				<ul class="dkarrow">
+					<li><a href="#">perl</a></li>
+					<li style="margin:0;"><a href="#">general</a></li>
+				</ul>
+			</div>
+			<div class="headerbox_orange">archives</div>
+			<div class="box_brown">
+				<ul class="dkarrow">
+					<li><a href="#">December</a></li>
+					<li style="margin:0;"><a href="#">January</a></li>
+				</ul>
+			</div>-->
+			<!--<div class="headerbox_orange">site login</div>
+			<div class="box_brown">
+				<table>
+					<tr>
+						<td style="padding: 10px;">
+							<div style="margin: 0 0 5px 0;">Username:<br/><input type="text" size="15"/></div>
+							<div style="margin: 0 0 5px 0;">Password:<br/><input type="password" size="15"/></div>
+							<div id="login"></div>
+						</td>
+					</tr>
+				</table>
+			</div>-->
+			<div class="headerbox_orange">delicious</div>
+            <div id="delicious" class="box_brown">
+            </div>
+			<div class="box_clear">
+                    RSS <a href="./rss.xml"><img src="./images/rss.png" border="0"/></a>
+                    <br/>
+    				<a href="http://validator.w3.org/check?uri=referer"><img
+        			src="http://www.w3.org/Icons/valid-xhtml10"
+        			alt="Valid XHTML 1.0 Strict" height="31" width="88" border="0" /></a>
+			</div>
+            <br/><br/>
+			<div id="tv"> 
+				<img src="./images/tv.png" width="150" height="209" />
+			</div>
+		</div>
+    ~;
+
+    return $html;
+}
+
+sub html_footer
+# Purpose:  Html for footer
+# Input:    1. Ref to self
+# Output:   1. String of html
+{
+    my ($self) = @_;    
+
+    return qq~
+		<div id="footer">&copy; 2009 Logan Bell. All rights reserved.</div>
+		</div>
+		<div id="squares"></div>
+	</div>
+</div>
+</body>
+</html>~;
+    
+}
+
+sub html_content
+# Purpose:  Abstract Method for content
+# Input:    1. Ref to self
+# Output:   1. String of html
+{
+    my ($self) = @_;    
+
+    return '';
 }
 
 sub new
@@ -59,6 +161,18 @@ sub new
     
 }
 
+sub meta
+# Purpose:  Accessor for meta information
+# Input:    1. Ref to self
+#           2. Hash of meta keywords
+# Output:   1. Hashref of meta keywords
+{
+    my ($self, %meta) = @_;
+
+    $self->{meta_hr} = \%meta;
+
+}
+
 sub output
 # Purpose:  Out all html markup
 # Input:    1. Ref to self
@@ -66,14 +180,87 @@ sub output
 {
     my ($self) = @_;
     
-    my $html = $self->html_header;
+    my $header  = $self->html_header;
+    my $content = $self->html_content;
+    my $footer  = $self->html_footer;
+
+    return $header . $content . $footer;
 
     
+}
+
+sub styles
+# Purpose:  Accessor for styles, keeps a unique list
+# Input:    1. Ref to self
+#           2. New css file
+# Output:   1. Hash ref of style sheet paths
+#           2. Hash ref of inline css styles
+{
+    my ($self, $css) = @_;
+
+    if ( $css =~ /\.css$/ )
+    {
+        $self->{_css_sheets_hr}->{$css} = 1;    
+    }
+    else
+    {
+        (my $minifed_css = $css) =~ s/[\r|\n|\s]//g;
+        $self->{_css_inline_hr}->{$minified_css} = $css;
+    }
+
+    return ( $self->{_css_sheets_hr}, $self->{_css_inline_hr} );
+}
+
+sub title
+# Purpose: Accessor for page title
+# Input:   1. Ref to self
+#          2. String of title
+# Output:  1. Title string
+{
+    my ($self, $title) = @_;
+
+    $self->{_title} = $title if $title;
+
+    return $self->{_title};
 }
 
 #-------------------------------------------
 # PRIVATE METHODS
 #-------------------------------------------
+
+sub _generate_css
+# Purpose:  Generates CSS markup
+# Input:    1. Ref to self
+# Output:   2. CSS markup for header
+{
+    my ($self) = @_;
+
+    my $css_markup = '';
+
+    $css_markup .= join "\n", map {'<link rel="stylesheet" href="' . $_ . '" type="text/css" />'} 
+                   keys %{$self->{_css_sheets_hr}};
+
+    $css_markup .= join "\n", map {'<style>' . $self->{_css_inline_hr}->{$_} . '</style>' }
+                   keys %{$self->{_css_inline_hr}};
+
+    return $css_markup;
+	
+}
+
+sub _generate_meta
+# Purpose:  Generates META markup
+# Input:    1. Ref to self
+# Output:   2. META markup for header
+{
+    my ($self) = @_;
+    my $meta_markup = '';
+	
+    $meta_markup .= join "\n", map { '<meta name="' . $_ . '" content="' . $self->{meta_hr}->{$_} . '" />' }
+                    sort keys %{$self->{meta_hr}};
+
+    return $meta_markup;
+
+}
 
 sub _init
 # Purpose:  Intializes the Abstract class
@@ -82,7 +269,20 @@ sub _init
 {
     my ($self, $params) = @_;
 
-    $self->{cgi_obj} = $params->{cgi_obj};
+    $self->{cgi_obj}            = $params->{cgi_obj};
+    $self->{_css_sheets_hr}     = {};
+    $self->{_css_inline_hr}     = {};
+
+    $self->title(DEFAULT_TITLE);
+    $self->styles(DEFAULT_STYLE_SHEET);
+
+    $self->meta( 
+                keywords    => META_KEYWORDS, 
+                description => META_DESCRIPTION, 
+                author      => META_AUTHOR, 
+                robots      => META_ROBOTS, 
+                copyright   => META_COPYRIGHT 
+               );
 
     return $self;
 }
--- t/test_controllers_abstractview.t
+++ t/test_controllers_abstractview.t
@@ -26,11 +26,13 @@ my $abstract_obj = Views::AbstractView->new( cgi_obj => $mock_cgi_obj );
 isa_ok($abstract_obj, 'Views::AbstractView', "Is the object View?");
 can_ok($abstract_obj, 'new');
 can_ok($abstract_obj, 'output');
+can_ok($abstract_obj, 'title');
+can_ok($abstract_obj, 'styles');
 
 my $results = $abstract_obj->output;
 $results =~ s/[\n|\r|\s]//g;
 
-my $control = 'foo<?xmlversion="1.0"encoding="utf-8"?><!DOCTYPEhtmlPUBLIC"-//W3C//DTDXHTML1.0Strict//EN""http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><htmlxmlns="http://www.w3.org/1999/xhtml"xml:lang="en"><head><title>LoganBell</title><metaname="keywords"content="loganbellloganbelllogblogbashperlruby"/><metaname="description"content="Yawn..."/><metaname="author"content="LoganBell"/><metaname="robots"content="all"/><metaname="copyright"content="Copyright2009(c)byLoganBell"/><linkrel="stylesheet"href="/styles/base.css"type="text/css"/></head><body><divid="page"><divid="header"><divid="title"><h1>Logan'sLog</h1></div><divid="subtitle">let'sconverse!</div></div><divid="yellowbar"></div><divid="wrapper"><%=import('navbar.html')%><divid="content">';
+my $control = qq~foo<?xmlversion="1.0"encoding="utf-8"?><!DOCTYPEhtmlPUBLIC"-//W3C//DTDXHTML1.0Strict//EN""http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><htmlxmlns="http://www.w3.org/1999/xhtml"xml:lang="en"><head><title>LoganBell</title><metaname="author"content="LoganBell"/><metaname="copyright"content="Copyright2009(c)byLoganBell"/><metaname="description"content="Yawn"/><metaname="keywords"content="LoganBell"/><metaname="robots"content="all"/><linkrel="stylesheet"href="/styles/base.css"type="text/css"/></head><body><divid="page"><divid="header"><divid="title"><h1>LoganBell</h1></div><divid="subtitle">let'sconverse!</div></div><divid="yellowbar"></div><divid="wrapper"><%=import('navbar.html')%><divid="content">~;
 
 is ($results,$control, "Do results match control?");
 
