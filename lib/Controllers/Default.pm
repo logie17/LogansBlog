a7c6386099894580a6a3ab97e2c90fff000af2d1
Logan Bell <loganbell@gmail.com>
2010-03-18 00:22:21 -0400

    Controllers::Default - Added index method

--- lib/Controllers/Default.pm
+++ lib/Controllers/Default.pm
@@ -3,13 +3,37 @@ package Controllers::Default;
 use Models::Entries;
 use base Controllers::AbstractController;
 
+use constant ENTRY_PER_PAGE => 5;
+use constant DEFAULT_PAGE   => 1;
+
 sub index
 {
-    my $entries = Models::Entries->get_entries;
+    my ($self) = @_;
+    my $cgi_obj     = $self->{cgi_obj};
 
-    my @entries_data = map { { title => $_->title, html => $_->html } } @$entries;
+    my $page_number;
 
-    return { entries => \@entries_data };
+    if ( $cgi_obj->param('parameters') && $cgi_obj->param('parameters')->[0] =~ /^\d+$/ )
+    {
+        $page_number = $cgi_obj->param('parameters')->[0];
+    }
+    else
+    {
+        $page_number = DEFAULT_PAGE;
+    }
+
+    my $offset      = ($page_number * ENTRY_PER_PAGE) - ENTRY_PER_PAGE;
+
+    my $parameters = $cgi_obj->param('parameters');
+
+    my $entries                 = Models::Entries->get_entries( sort_by => 'post_date DESC', limit => ENTRY_PER_PAGE, offset => $offset );
+    my $current_entry_count     = scalar @{$entries};
+    my $total_entry_count       = Models::Entries->get_entries_count;
+
+    my $previous_page_number   = $page_number > 1 ? $page_number - 1 : '';
+    my $next_page_number       = ($page_number * ENTRY_PER_PAGE) < $total_entry_count ? $page_number + 1 : '';
+
+    return { entries => $entries, next_page_number => $next_page_number, previous_page_number => $previous_page_number };
 
 }
 
