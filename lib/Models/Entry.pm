bcb820e8be172f951445826d926f1acc06ed7895
Logan Bell <loganbell@gmail.com>
2010-03-18 00:24:36 -0400

    Models::Entry - Added nonpersistent_columns dt_obj and post_date

--- lib/Models/Entry.pm
+++ lib/Models/Entry.pm
@@ -2,6 +2,7 @@ package Models::Entry;
 
 use base Rose::DB::Object;
 use Models::DB;
+use DateTime;
 
 use strict;
 use warnings;
@@ -10,10 +11,14 @@ use warnings;
 __PACKAGE__->meta->setup
 (
     table   => 'entries',
-    columns => [ qw(id title html ) ],
+    columns => [ 'id', 'title', 'html', post_date => {type => 'timestamp'} ],
     pk_columns => 'id'
 
 );
+
+#__PACKAGE__->meta->add_nonpersistent_columns('dt_obj');
+
+
 
 sub init_db { Models::DB->new }
 
