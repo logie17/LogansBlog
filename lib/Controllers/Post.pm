3fa9695d0b240ffc321c8dea68fd2f900d473a2d
Logan Bell <loganbell@gmail.com>
2012-02-09 18:04:08 -0500

    reformatted a bit.

--- lib/Controllers/Post.pm
+++ lib/Controllers/Post.pm
@@ -8,16 +8,31 @@ sub view
     my ($self) = @_;
 
     my $cgi_obj = $self->{cgi_obj};
-    my $id;
+    my ($id, $title);
 
-    if ( $cgi_obj->param('parameters') && $cgi_obj->param('parameters')->[0] =~ /^\d+$/ )
-    {
+    if ( $cgi_obj->param('parameters') && $cgi_obj->param('parameters')->[0] =~ /^\d+$/ ) {
         $id = $cgi_obj->param('parameters')->[0];
+    } elsif ( $cgi_obj->param('parameters') && $cgi_obj->param('parameters')->[0] =~ /^[^\d]+$/ ) {
+        $title = $cgi_obj->param('parameters')->[0]; 
     }
 
-    if ( my $entries = Models::Entries->get_entries( where => [ id => $id ] ) )
-    {
-        return { entries => $entries };
+    if ( $id ) {
+        if ( my $entries = Models::Entries->get_entries( where => [ id => $id ] ) ) {
+            return { entries => $entries };
+        }
+    }
+    if ( $title ) {
+        $title =~ s/\-/ /g;
+        my $entries = Models::Entries->get_objects_from_sql(
+            sql => qq[
+                SELECT id, title, html, post_date FROM entries WHERE title = ? COLLATE NOCASE
+            ],
+            args => [ lc $title ]
+        );
+
+        if ( $entries ) {
+            return { entries => $entries };
+        }
     }
 }
 
