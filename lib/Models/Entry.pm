2d233363c3c9f86442bfdb61953d154d3f2271ac
Logan Bell <loganbell@gmail.com>
2012-02-10 17:47:09 -0500

    Added relationship information for tags.

--- lib/Models/Entry.pm
+++ lib/Models/Entry.pm
@@ -4,34 +4,39 @@ use base Rose::DB::Object;
 use Models::DB;
 use DateTime;
 use HTML::Entities qw(encode_entities);
+use Models::Entries;
 
 use strict;
 use warnings;
-
 
 __PACKAGE__->meta->setup
 (
     table   => 'entries',
     columns => [ 'id', 'title', 'html', post_date => {type => 'timestamp'} ],
-    pk_columns => 'id'
+    pk_columns => 'id',
+    relationships =>
+    [
+      tags =>
+      {
+        type      => 'many to many',
+        map_class => 'Models::EntriesTagsMap',
+      },
+    ],
 
 );
 
-sub init_db 
-{ 
+sub init_db { 
     Models::DB->new;
 }
 
-sub friendly_post_date 
-{
+sub friendly_post_date {
     my $self      = shift;
     my $friendly  = $self->post_date(format => '%m/%d/%Y');
     $friendly     =~ s/\s1$//g;
     return $friendly;
 }
 
-sub escaped_html
-{
+sub escaped_html {
     my $self = shift;
     return encode_entities($self->html);
 }
@@ -43,5 +48,11 @@ sub seo_title {
     return lc $seo_title;
 }
 
+sub all_tags {
+    my $self = shift;
+    my $id = $self->id;
+    my $entries = Models::Entries->get_entries( query => [ id => $id ] )->[0];
+    return $entries->tags;
+}
 
 1;
