570d39dececbcda868a42dc0793a2f1aebfe236f
Logan Bell <loganbell@gmail.com>
2012-02-10 17:46:46 -0500

    Added logic to support tags.

--- lib/Controllers/Default.pm
+++ lib/Controllers/Default.pm
@@ -13,29 +13,33 @@ sub index
 
     my $page_number;
 
-    if ( $cgi_obj->param('parameters') && $cgi_obj->param('parameters')->[0] =~ /^\d+$/ )
-    {
+    if ( $cgi_obj->param('parameters') && $cgi_obj->param('parameters')->[0] =~ /^\d+$/ ) {
         $page_number = $cgi_obj->param('parameters')->[0];
     }
-    else
-    {
+    else {
         $page_number = DEFAULT_PAGE;
+    }
+
+    my %query;
+    if ( my $tag = $cgi_obj->param('tag') ) {
+        $query{query} = [ 't3.tag'   => $tag ];
     }
 
     my $offset      = ($page_number * ENTRY_PER_PAGE) - ENTRY_PER_PAGE;
 
     my $parameters = $cgi_obj->param('parameters');
 
-    my $entries                 = Models::Entries->get_entries( sort_by => 'post_date DESC', limit => ENTRY_PER_PAGE, offset => $offset );
+    my $entries                 = Models::Entries->get_entries( with_objects => ['tags'], sort_by => 'post_date DESC', limit => ENTRY_PER_PAGE, offset => $offset, %query);
     my $current_entry_count     = scalar @{$entries};
     my $total_entry_count       = Models::Entries->get_entries_count;
 
     my $previous_page_number   = $page_number > 1 ? $page_number - 1 : '';
-    my $next_page_number       = ($page_number * ENTRY_PER_PAGE) < $total_entry_count ? $page_number + 1 : '';
+    my $next_page_number       = ($page_number * ENTRY_PER_PAGE) < $total_entry_count && $current_entry_count >= ENTRY_PER_PAGE ? $page_number + 1 : '';
 
     return { entries => $entries, next_page_number => $next_page_number, previous_page_number => $previous_page_number };
 
 }
+
 
 
 1;
